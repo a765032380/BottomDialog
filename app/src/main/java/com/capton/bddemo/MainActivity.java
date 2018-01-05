28927235bec1ab017b9a5c4acbfe1183155b32f4
ccapton <chenweibin1125@foxmail.com>
2018-01-05 12:17:27 +0800

    增添一个示例view

--- app/src/main/java/com/capton/bddemo/MainActivity.java
+++ app/src/main/java/com/capton/bddemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.capton.bddemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Switch;
 
 import com.capton.bd.BottomDialog;
@@ -24,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
                  // 1、新建Builder对象
                  BottomDialog.Builder builder = new BottomDialog.Builder(MainActivity.this);
                  // 2、设置布局文件
-                 builder.setContentView(R.layout.layout_bottom_demo);
+                 builder.setContentView(R.layout.layout_bottom_demo2);
                  // 3、获取BottomDialog对象
                  bottomDialog = builder.create();
                  // 4、显示BottomDialog
@@ -32,7 +33,8 @@ public class MainActivity extends AppCompatActivity {
 
                  // 5、获取布局View对象,用于设置布局内的控件事件
                  View bottomView = bottomDialog.getContentView();
-                 bottomView.setOnClickListener(new View.OnClickListener() {
+                 ImageView close = bottomView.findViewById(R.id.close);
+                 close.setOnClickListener(new View.OnClickListener() {
                      @Override
                      public void onClick(View v) {
                          bottomDialog.dismiss();
